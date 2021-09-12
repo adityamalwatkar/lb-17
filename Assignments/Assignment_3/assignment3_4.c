a9b71c2b642bdff7327eb8ee1c88ab54d55bcd31
adityamalwatkar <adimalwatkar16@gmail.com>
2021-09-12 13:30:56 +0530

    Commiting assignement_3 and assignment_4

---
+++ Assignments/Assignment_3/assignment3_4.c
@@ -0,0 +1,45 @@
+/*5. Accept on character from user and check whether that character is vowel
+(a,e,i,o,u) or not.*/
+
+#include<stdio.h>
+#include<stdbool.h>
+
+typedef int BOOL
+
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkVowel(char ch)
+{
+	if(ch == 'a' && ch == 'A' && ch == 'e'&& ch == 'E' && ch == 'i' && ch == 'I' && ch == 'o' && ch == 'O'&& ch == 'u'&& ch == 'U')
+
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+int main()
+{
+	char cValue = '\0';
+	BOOL bRet = FALSE;
+
+	printf("Enter charecter: ");
+	scanf("%c", &cValue);
+
+	bRet = ChkVowel(cValue);
+
+	if(bRet == TRUE)
+	{
+		printf("It is Vowel\n");
+	}
+	else
+	{
+		printf("It is not Vowel\n");
+	}
+
+	return 0;
+}
